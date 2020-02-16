e04e1d6e4a19dc8473795cb8c6854ae554f64c71
George <gorgegames1995@gmail.com>
2020-02-16 04:53:11 -0500

    Changed BK -> Cam

--- app/Models/Recruiting/Status.php
+++ app/Models/Recruiting/Status.php
@@ -20,14 +20,9 @@ class Status extends Model
     public static function buildRecruitmentMessage(Nation $nation) : string
     {
         $topMessage = "Hey {$nation->leader},\n\n";
-        $endMessage = "\n\nIf you would like to join BK, then I think you made the right choice. Getting the process of joining started is easy. Just follow these steps:
--- Join our alliance in-game here: https://politicsandwar.com/alliance/id=4937
--- [link=http://bkpw.net/register/?core_pfield_11={$nation->nID}]Register an account on our forums here[/link] (We've filled out the nation ID field for you) 
--- Submit an application. [link=http://bkpw.net/topic/356-how-to-apply-to-the-black-knights/]Instructions here[/link] 
+        $endMessage = "\n\nI
 
-However, if you feel that BK isn't for you, I wish you the best of luck and I hope you are happy with the alliance you chose. 
-
-As always, if you have any questions about BK or the game, in general, let me know and I will get back to you as soon as possible. 
+As always, if you have any questions about Camelot or the game, in general, let me know and I will get back to you as soon as possible.
 
 Thanks!";
 
