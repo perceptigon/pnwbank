e5140831527b4a4bc19d5851be85f5d11e94558e
George <gorgegames1995@gmail.com>
2020-02-16 05:47:27 -0500

    Turned on recruit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -74,6 +74,7 @@
 
                             <ul class="dropdown-menu" role="menu">                           
                                 <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
+                                <li><a href="{{ url('/user/dashboard') }}"><i class="fa fa-btn fa-sign-out"></i>Dashboard</a></li>
                             </ul>
                         </li>
                     @endif
