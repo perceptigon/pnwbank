5eae72462984b45148b7f947f1e883dcf7edd6c8
Epimetheus <JonJefferis42@gmail.com>
2020-02-23 19:16:28 +1300

    Cam Flag

--- resources/views/ia/apply.blade.php
+++ resources/views/ia/apply.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-    <p style="text-align:center;padding-top:10px;"><img src="{{ url("/images/tibernet_logo.png") }}" alt="Tibernet Logo" style="width:400px;height:150px;"><img src="{{ url("/images/flag.png") }}" alt="BK Flag" style="width:300px;height:150px;"></p>
+    <p style="text-align:center;padding-top:10px;"><img src="{{ url("/images/tibernet_logo.png") }}" alt="Tibernet Logo" style="width:400px;height:150px;"><img src="{{ url("https://politicsandwar.com/uploads/abba90655c97307b46314e76e68da98008e2ff2c641.png") }}" alt="Cam Flag" style="width:300px;height:150px;"></p>
 
     <div class="row">
         <div class="col-md-3"></div>
