c26a4a4d39e24c69d49d4d29ed985200cad8d9aa
George <gorgegames1995@gmail.com>
2020-03-16 05:24:34 -0400

    Adding forums

--- app/Forums/ForumFields.php
+++ app/Forums/ForumFields.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class ForumFields extends Model
 {
     protected $connection = "forums2";
-    protected $table = "ipb_core_pfields_content";
+    protected $table = "core_pfields_content";
 }
--- app/Forums/ForumMembers.php
+++ app/Forums/ForumMembers.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class ForumMembers extends Model
 {
     protected $connection = "forums2";
-    protected $table = "ipb_core_members";
+    protected $table = "core_members";
 }
