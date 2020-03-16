93e82363caa3a5a7656fad347f0ac14007bdc92a
George <gorgegames1995@gmail.com>
2020-03-16 05:00:37 -0400

    Adding forums

--- app/Forums/ForumFields.php
+++ app/Forums/ForumFields.php
@@ -6,6 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class ForumFields extends Model
 {
-    protected $connection = "forums";
+    protected $connection = "forums2";
     protected $table = "ipb_core_pfields_content";
 }
