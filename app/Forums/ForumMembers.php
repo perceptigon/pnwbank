57b1720f518872e13697c414f9c7a4ceb1ca5e3a
George <gorgegames1995@gmail.com>
2020-03-16 05:01:41 -0400

    Adding forums

--- app/Forums/ForumMembers.php
+++ app/Forums/ForumMembers.php
@@ -6,6 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class ForumMembers extends Model
 {
-    protected $connection = "forums";
+    protected $connection = "forums2";
     protected $table = "ipb_core_members";
 }
