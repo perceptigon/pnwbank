f7b9d7e92919c0587ff6145869cdb7a7483e9cec
mrblackbird <35431260+mrblackbird@users.noreply.github.com>
2020-04-30 11:13:28 +0100

    line breaks v4

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,5 +8,4 @@
         <div class="col-lg-6">
             <a href="{{ url("/signin") }}" class="btn btn-xl btn-link homeButton">Sign In</a>
         </div>
-    </div><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-@endsection
+    </div><br><br><br><br><br><br><br><br><br>
