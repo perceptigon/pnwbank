16a81db95f3e9023d2f474ee92324bcdd39acbea
mrblackbird <35431260+mrblackbird@users.noreply.github.com>
2020-05-01 07:44:59 +0100

    no

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,7 @@
     <div class="navbar navbar-default">
         <div class="container">
             <div class="navbar-header">
-                <a href="{{ url("/") }}" class="navbar-brand"><img src="https://i.ibb.co/4WJMJBJ/imageedit-1-2849241818.png" width="43" height="56" title="RnCo Logo" alt="RnCo Logo" />Rothschilds & Co.{{ App::environment('local') ? "(Dev)" : "" }}</a>
+            <img src="https://i.ibb.co/4WJMJBJ/imageedit-1-2849241818.png" width="43" height="56" title="RnCo Logo" alt="RnCo Logo" /><a href="{{ url("/") }}" class="navbar-brand">Rothschilds & Co.{{ App::environment('local') ? "(Dev)" : "" }}</a>
                 <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#navbar-main">
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
