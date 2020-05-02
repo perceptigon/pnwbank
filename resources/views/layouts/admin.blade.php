6054335a44fa55b8bb0c34c7cac809643676e6b0
mrblackbird <35431260+mrblackbird@users.noreply.github.com>
2020-05-02 02:06:41 +0100

    admin title

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,7 +1,7 @@
 <html><head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>{{ App::environment('local') ? "(Dev)" : "" }} Rothschilds & Co.</title>
+    <title>{{ App::environment('local') ? "(Dev)" : "" }} RnCo Admin</title>
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 
     <link href="{{ url("lib/bootstrap/default/css/bootstrap.min.css") }}" rel="stylesheet">
