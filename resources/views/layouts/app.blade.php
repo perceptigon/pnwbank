d5fdec8bee55c5992a3317ea71906a77b99050fc
mrblackbird <35431260+mrblackbird@users.noreply.github.com>
2020-05-02 09:26:06 +0100

    asd

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,7 +36,7 @@
                     </li>
                     <li><a href="{{ url("/ia/apply") }}">About</a></li>
 
-                    <li><br> <br>RnCo Server Time <?php echo date("D M d, Y G:i a"); ?></li>
+               
 
                 </ul>
                
@@ -56,7 +56,7 @@
                             <ul class="dropdown-menu" role="menu">                           
                                 <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
                                 <li><a href="{{ url('/user/dashboard') }}"><i class="fa fa-btn fa-sign-out"></i>Dashboard</a></li>
-                            </ul>
+                            </ul>     <li>>RnCo Server Time <?php echo date("D M d, Y G:i a"); ?></li>
                         </li>
                     @endif
                 </ul>
