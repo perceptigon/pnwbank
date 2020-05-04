5efc4f54733e1ba4c0103eba49d494645a20e02d
mrblackbird <35431260+mrblackbird@users.noreply.github.com>
2020-05-04 17:07:07 +0100

    sad

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
+
+<p style="text-align:center;padding-top:10px;">
+<img src="https://i.ibb.co/T0MZqvH/imageedit-19-6358665258.png" width="330" height="245" title="Rothschilds & Co." alt="" /></p>
     <div class="row">
         <div class="col-lg-6">
-            <a href="{{ url("/ia/apply") }}" class="btn btn-xl btn-link homeButton">Apply</a>
+            <a href="{{ url("/ia/apply") }}" class="btn btn-xl btn-link homeButton">About</a>
         </div>
         <div class="col-lg-6">
-            <a href="{{ url("/signin") }}" class="btn btn-xl btn-link homeButton">Sign In</a>
+            <a href="{{ url("https://politicsandwar.fandom.com/wiki/Rothschild_Family") }}" class="btn btn-xl btn-link homeButton">Wiki</a>
         </div>
         <div class="col-lg-6">
             <a href="{{ url("/accounts") }}" class="btn btn-xl btn-link homeButton">Accounts</a>
@@ -20,5 +23,5 @@
         <div class="col-lg-6">
             <a href="{{ url("/outsidetransfer") }}" class="btn btn-xl btn-link homeButton">Withdraw to an alliance</a>
         </div>
-    </div><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+    </div>
 @endsection
