63f4866ad8650f5a544a5a988c1a0b9824486df7
mrblackbird <35431260+mrblackbird@users.noreply.github.com>
2020-06-29 21:53:59 +0100

    paragraph mistakes

--- resources/views/ia/apply.blade.php
+++ resources/views/ia/apply.blade.php
@@ -4,8 +4,10 @@
 <p style="text-align:center;padding-top:10px;">
 <a href="https://discord.gg/ZmBSJnJ"><img src="{{ url("https://i.ibb.co/p6D7NpS/imageedit-7-2438352295.png") }}" alt="Join Banque Lumiére Discord server" style="width:283px;height:71px;"></a>
 <br><br>
-Banque Lumiére prides itself in being the technologically most advanced bank in all of Orbis. 
+Banque Lumiére prides itself in being the technologically most advanced bank in all of Orbis.
+<br>
 With our automated banking system customers can access their funds anytime and wherever, without any help of a banker.
+<br>
 Aside from our unique banking system, we also offer amazing financial products to meet your various expectations either as an alliance, a normal player or even a trader, we have just the product that's suitable for you.
 <br><br>
 <img src="{{ url("https://i.ibb.co/VxRQxMZ/imageedit-2-4291056806.png") }}" alt="Rothschild Family logo" style="width:300px;height:269px;"><br> 
