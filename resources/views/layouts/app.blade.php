3ef764b26ce58d03abb8a240ed916807f2535515
mrblackbird <35431260+mrblackbird@users.noreply.github.com>
2020-10-25 16:26:26 +0000

    v6.1

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,19 +32,20 @@
 
 
 
-                    
+                    <?php
 $t = date("i");
 
 @if ($t < "05") 
-  <li><a href="{{ url("\") }}"><font color="E60000"><strong>A turn is in progress!</strong> Transactions wont work. Time <u> . date("h:i:s A") . </u></font></a></li>
+  '<li><a href="{{ url(\"\") }}"><strong><font color=\"E60000\">A turn is in progress!</strong> Transactions won\'t work. Time <u>' . date("h:i:s A") . '</u></font></a></li>';
  @else 
     @if($t > "55") {
-  <li><a href="{{ url("\") }}"><font color="E60000"><strong>A turn is in progress!</strong> Transactions wont work. Time <u> . date("h:i:s A") . </u></font></a></li>
+  '<li><a href="{{ url(\"\") }}"><strong><font color=\"E60000\">A turn is in progress!</strong> Transactions won\'t work. Time <u>' . date("h:i:s A") . '</u></font></a></li>';
  @else
         @if
-  <li><a href="{{ url("\") }}">RnCo Server Time <u> . date("l jS \of F Y h:i:s A") . </u></a></li>
+  '<li><a href="{{ url(\"\") }}">RnCo Server Time <u>' . date("l jS \of F Y h:i:s A") . '</u></a></li>';
+
+?>
         @endif
-       
                 </ul>
                
                 <ul class="nav navbar-nav navbar-right">
