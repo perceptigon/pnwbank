a810c337bc8270c2f86757c7fb6fb6d61056678e
mrblackbird <35431260+mrblackbird@users.noreply.github.com>
2020-12-01 16:35:57 +0000

    accounts sorted alphabetically by leader name

--- resources/views/bankAccounts/templates/transfer.blade.php
+++ resources/views/bankAccounts/templates/transfer.blade.php
@@ -35,43 +35,44 @@
                             <option value="927"> Sovereign Wealth Fund</option> 
 
                             <optgroup label="ONN Accounts">
-<option value="1054"> 91386  - ONN - Parish</option> 
-<option value="1055"> 227732 - ONN - The Shashwat</option> 
-<option value="1056"> 217405 - ONN - Soprano</option> 
 <option value="1057"> 226261 - ONN - Lyonia</option> 
+<option value="1054"> 91386  - ONN - Parish</option> 
+<option value="1056"> 217405 - ONN - Soprano</option> 
+<option value="1055"> 227732 - ONN - The Shashwat</option> 
 
                             <optgroup label="Banque Lumiére Accounts">
-<option value="927"> 187328 - Freyu - Vander Lord's account</option>
-<option value="933"> 49745 - whizzy - Banque Lumiere Reserve Account</option>
-<option value="1051"> 49745 - whizzy - Sui Savings Account</option>
-<option value="957"> 164617 - James Heron - -C- Account</option>
-<option value="974"> 175001 - Blackbird - The World's Debt</option>
-<option value="1062"> 175001 - Blackbird - IB Stock Exchange</option>
-<option value="983"> 91386 - Parrish - Orbis International Corporation </option>
-<option value="985"> 163371 - Konungariket Sverige - TKBO Storage</option>
-<option value="989"> 217537 - Benj - Dividends</option>
-<option value="990"> 217537 - Benj - Swing reserve 3</option>
-<option value="991"> 86606 - Haris the Guy - Dividends</option>
-<option value="992"> 193189 - Arkin - Dividends</option>
-<option value="995"> 19818 - BlackAsLight - Dividends</option>
-<option value="996"> 190999 - Damon - Savings account</option>
-<option value="999"> 190999 - Damon - Zeal Royal Casino</option>
-<option value="1003"> 226261 - Lysander the Great - Lyonia </option>
-<option value="1009"> 8360 - Greene - Taith Group</option>
-<option value="1011"> 91386 - Parrish - dividends</option>
-<option value="1013"> 190999 - Damon - Revenue</option>
-<option value="1017"> 172844 - Lord Vader - RSS Flipping</option>
-<option value="1020"> 237315 - Jorge Robledo - Savings</option>
-<option value="1022"> 195377 - Chimpy_Fire - Original Chimpy</option>
-<option value="1024"> 148936 - Dabigbluewhale - Dabigbluewhale</option>
-<option value="1025"> 229318 - TigerFire - Dividends</option>
-<option value="1026"> 8360 - Greene - Taith Group - Share Dividends</option>
-<option value="1027"> 233135 - Keoneatyou - Keoneatyou</option>
-<option value="1028"> 217537 - Benj - The Orbit Stock Exchange</option>
-<option value="1030"> 237768 - Anis - Anis</option>
-<option value="1031"> 146200 - Tamunominini - Xantrooph2</option>
-<option value="1032"> 190999 - Damon - Zeal Royal Casino (1)</option>
+<option value="1030"> 237768 - Anis - Anis</option>     
+<option value="992"> 193189 - Arkin - Dividends</option>     
+<option value="989"> 217537 - Benj - Dividends</option>     
+<option value="990"> 217537 - Benj - Swing reserve 3</option>   
+<option value="1028"> 217537 - Benj - The Orbit Stock Exchange</option>  
+<option value="995"> 19818 - BlackAsLight - Dividends</option>     
+<option value="974"> 175001 - Blackbird - The World's Debt</option>   
+<option value="1062"> 175001 - Blackbird - IB Stock Exchange</option>   
+<option value="1022"> 195377 - Chimpy_Fire - Original Chimpy</option>    
+<option value="1024"> 148936 - Dabigbluewhale - Dabigbluewhale</option>     
+<option value="996"> 190999 - Damon - Savings account</option>    
+<option value="999"> 190999 - Damon - Zeal Royal Casino</option>   
+<option value="1013"> 190999 - Damon - Revenue</option>     
+<option value="1032"> 190999 - Damon - Zeal Royal Casino (1)</option>  
+<option value="927"> 187328 - Freyu - Vander Lord's account</option>   
+<option value="1009"> 8360 - Greene - Taith Group</option>    
+<option value="1026"> 8360 - Greene - Taith Group - Share Dividends</option> 
+<option value="991"> 86606 - Haris the Guy - Dividends</option>     
+<option value="957"> 164617 - James Heron - -C- Account</option>    
+<option value="1020"> 237315 - Jorge Robledo - Savings</option>     
+<option value="1027"> 233135 - Keoneatyou - Keoneatyou</option>     
+<option value="985"> 163371 - Konungariket Sverige - TKBO Storage</option>    
+<option value="1017"> 172844 - Lord Vader - RSS Flipping</option>    
+<option value="1003"> 226261 - Lysander the Great - Lyonia </option>    
+<option value="983"> 91386 - Parrish - Orbis International Corporation </option>  
+<option value="1011"> 91386 - Parrish - dividends</option>     
 <option value="1033"> 246639 - Roald - The Bank Of Roaldlands offshore account</option>
+<option value="1031"> 146200 - Tamunominini - Xantrooph2</option>     
+<option value="1025"> 229318 - TigerFire - Dividends</option>     
+<option value="933"> 49745 - whizzy - Banque Lumiere Reserve Account</option>  
+<option value="1051"> 49745 - whizzy - Sui Savings Account</option>   
+
 
 
 
