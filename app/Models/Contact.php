8b3ebd3c8fd360b236207b023485b8579f05d68b
mrblackbird <35431260+mrblackbird@users.noreply.github.com>
2020-12-26 21:57:49 +0000

    v10.9.3.9

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -52,7 +52,6 @@ class Contact extends Model
     public static function createReq(Request $request)
     {
         $req = new self;
-        $req->discord = $request->discord;
         $req->nID = $request->nID;
         $req->leader = $request->leader;
         $req->message = $request->message;
