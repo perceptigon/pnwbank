dcc5d946569906e53dbd3445cc1f8f0b6a738d1d
mrblackbird <35431260+mrblackbird@users.noreply.github.com>
2020-12-26 21:39:28 +0000

    v9.3

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -7,20 +7,7 @@
 			<form method="post">
                 <div class="form-group">
 
-                <div class="form-group{{ $errors->has('nID') ? ' has-error' : '' }}">
-                    <label class="col-md-4 control-label">Nation ID</label>
-
-                    <div class="col-md-6">
-                        <input type="number" class="form-control" name="nID" value="{{ old('nID') }}">
-
-                        @if ($errors->has('nID'))
-                            <span class="help-block">
-                                <strong>{{ $errors->first('nID') }}</strong>
-                            </span>
-                        @endif
-                        <span class="help-block">The nation ID is the numbers at the end of the URL when you view your nation. EX: https://politicsandwar.com/nation/id=<strong>XXXXX</strong></span>
-                    </div>
-                </div>
+          
                 <div class="form-group{{ $errors->has('discord') ? ' has-error' : '' }}">
                     <label class="col-md-4 control-label">Discord username</label>
 
