b21a01dedef6fe94baa6ac031fc1b59e6a66f14e
mrblackbird <35431260+mrblackbird@users.noreply.github.com>
2021-01-08 14:26:21 +0000

    UPDATED BLACK acc trasnsfer

--- resources/views/bankAccounts/templates/transfer.blade.php
+++ resources/views/bankAccounts/templates/transfer.blade.php
@@ -28,8 +28,8 @@
                             </optgroup>
 
                             <optgroup label="Rothschild Family Accounts">
-                            <option value="1063"> Blackbird - IB Revenue & Customs</option> 
-                            <option value="1096"> Factorian Kallio - FLPvR</option>
+                            <option value="1100"> Blackbird - BLACK</option> 
+                            <option value="1093"> Factorian Kallio - FLPvR</option>
                             <option value="1091"> Loki - Loki investment fund</option>  
                             <option value="903"> Naruu - Frosties magical money printer</option> 
                             <option value="922"> Whizzy - Savings account 1</option> 
