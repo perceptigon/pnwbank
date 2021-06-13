c426a6c79fc7a2821eea02f1d12b12a2b914cf2c
Chris Blackbird <lv@kriss.lv>
2021-06-13 10:18:35 +0000

    removed the whole section for delete button

--- resources/views/bankAccounts/templates/create.blade.php
+++ resources/views/bankAccounts/templates/create.blade.php
@@ -20,29 +20,5 @@
             </div>
         </div>
     </div>
-    <div class="col-md-6">
-        <div class="panel panel-danger">
-            <div class="panel-heading">
-                '<'h3 class="panel-title">Delete Account</h3'>'
-            </div>
-            <div class="panel-body">
-                <form method="post" onsubmit="return confirm('Are you sure you want to delete this account?')">
-                    <div class="form-group">
-                        <label class="control-label" for="del_accountName">Account Name</label>
-                        <div class="input-group">
-                            <select class="form-control" id="del_accountName" name="accountID">
-                                @foreach ($accounts as $account)
-                                    <option value="{{ $account->id }}">{{ $account->name }} - ${{ number_format($account->money, 2) }}</option>
-                                @endforeach
-                            </select>
-                            <span class="input-group-btn">
-                        <input type="submit" class="btn btn-danger" value="Delete" name="deleteAccount">
-                    </span>
-                        </div>
-                    </div>
-                    {{ csrf_field() }}
-                </form>
-            </div>
-        </div>
-    </div>
+  
 </div>
